4fe1cdf17268c11b1b7b6d5593402670c5939935
omarftt <33913680+omarftt@users.noreply.github.com>
2022-02-09 17:39:47 -0500

    'CourseSolutionsSchemas_v1' (#3)
    * 'CourseSolutionsSchemas_v1'
    
    * 'Course-Schema-types-added'

--- src/models/course.models.js
+++ src/models/course.models.js
@@ -1,6 +1,22 @@
 import { model, Schema } from "mongoose";
 
 const courseSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    topic: {
+        type: String,
+        required: true,
+    },
+    questions: {
+        type: [{ type: Schema.Types.ObjectId, ref: "Question" }],
+        required: true,
+    },
+    completed: {
+        type: Boolean,
+        required: true,
+    }
   
 });
 
