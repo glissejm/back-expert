1b3bf0f0e9fdb9066126f87d361eea05cbbe15c6
Elias Jurado <ridimdev@tuta.io>
2022-02-09 17:57:26 -0500

    simulacrum

--- src/models/simulacrum.models.js
+++ src/models/simulacrum.models.js
@@ -1,7 +1,14 @@
 import { model, Schema } from "mongoose";
 
 const simulacrumSchema = new Schema({
-  
+    name: {
+        type: String,
+        required: true,
+    }, 
+    questions_id: {
+        type: [String],
+        required: true,
+    }, 
 });
 
 const Simulacrum = model("Simulacrum", simulacrumSchema);
