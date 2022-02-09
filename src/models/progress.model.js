ef2503ddf025884c61358f746b3a48b555e63cab
Glisse Jorge <glisse.jorge.27@unsch.edu.pe>
2022-02-09 18:17:41 -0500

    add model progress

--- src/models/progress.model.js
+++ src/models/progress.model.js
@@ -1,7 +1,14 @@
 import { model, Schema } from "mongoose";
 
 const progressSchema = new Schema({
-  
+    question: {
+        type: [{ type: Schema.Types.ObjectId, ref: "Question" }],
+        required:true,
+    },
+    simulacrum: {
+        type: [{ type: Schema.Types.ObjectId, ref: "Simulacrum" }],
+        required: true,
+    },
 });
 
 const Progress = model("Progress", progressSchema);
