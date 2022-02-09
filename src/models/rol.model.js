7896c6d1225fa70ca877a823d839bcbca149d718
Glisse Jorge <glisse.jorge.27@unsch.edu.pe>
2022-02-09 18:14:23 -0500

    add model rol

--- src/models/rol.model.js
+++ src/models/rol.model.js
@@ -1,7 +1,13 @@
 import { model, Schema } from "mongoose";
 
 const rolSchema = new Schema({
-  
+    role: {
+        type: String,
+        required: true,
+    },
+    db: {
+        type: String,
+    },
 });
 
 const Rol = model("Rol", rolSchema);
