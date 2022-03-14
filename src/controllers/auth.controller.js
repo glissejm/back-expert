d45327378fe77c939c9e806804549514deb1a4ff
leuu-1998 <lmoinah@uni.pe>
2022-03-14 12:20:08 -0500

    add verify token controller(once you open the app frontend) and google auth, login and register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.model";
 import bcrypt from "bcrypt";
+import { OAuth2Client } from "google-auth-library";
 import { newToken } from "../utils/middlewares/verifyToken";
 
 export async function signUp(req, res) {
@@ -63,6 +64,110 @@ export async function signIn(req, res) {
   }
 }
 
+//auth with google
+export async function loginGoogle(req, res) {
+  try {
+    const { token } = req.body;
+    const client = new OAuth2Client(process.env.GOOGLE_KEY_ID);
+    //we don't store CLIENT_ID is the 3rd party application
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.CLIENT_ID,
+    });
+    //the json to ticket.getPayload is:
+
+    /* iss: 'accounts.google.com',
+      azp: '810139574456-5o6qassgd0ko3pn57lfrawfrwaffaf7m1.apps.googleusercontent.com',
+      aud: '81139574456-5o6qafrawfatgryhryl43slsfp2ssf7m1.apps.googleusercontent.com',
+      sub: '117319928916955291616',
+      email: 'email_user',
+      email_verified: true,
+      at_hash: 'fYB4sBNfrQ7LVlgvB7wIzA',
+      name: 'Complete name with last name',
+      picture: 'URL_image_profile',
+      given_name: 'Nombre',
+      family_name: 'Apellidos separados',
+      locale: 'es',
+      iat: 1647232408,
+      exp: 1647236008,
+      jti: '78c55824479ef33c3545db436f4c3df98bae4fc2'
+    } */
+    const { email } = ticket.getPayload();
+    //emails are unique for each user
+    const user = await User.findOne({ email });
+    //what if there's not user in the db
+    if (!user) {
+      return res.status(400).json({
+        message: "El usuario no existe, por favor, regístrese en la aplicación",
+      });
+    }
+    //create a token
+    const tokenJWT = newToken(user);
+    res
+      .status(201)
+      .cookie("SECURE_ACCESS", tokenJWT, {
+        httpOnly: true,
+        path: "/",
+        secure: true,
+      })
+      .json({ name: user.name, email: user.email });
+  } catch (e) {
+    res.status(400).json({ message: "No se pudo iniciar sesión con google" });
+  }
+}
+//register with google
+export async function registerGoogle(req, res) {
+  try {
+    const { token } = req.body;
+    const client = new OAuth2Client(process.env.GOOGLE_KEY_ID);
+    //we don't store CLIENT_ID is the 3rd party application
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.CLIENT_ID,
+    });
+    //the json to ticket.getPayload is:
+
+    /* iss: 'accounts.google.com',
+      azp: '810139574456-5o6qassgd0ko3pn57lfrawfrwaffaf7m1.apps.googleusercontent.com',
+      aud: '81139574456-5o6qafrawfatgryhryl43slsfp2ssf7m1.apps.googleusercontent.com',
+      sub: '117319928916955291616',
+      email: 'email_user',
+      email_verified: true,
+      at_hash: 'fYB4sBNfrQ7LVlgvB7wIzA',
+      name: 'Complete name with last name',
+      picture: 'URL_image_profile',
+      given_name: 'Nombre',
+      family_name: 'Apellidos separados',
+      locale: 'es',
+      iat: 1647232408,
+      exp: 1647236008,
+      jti: '78c55824479ef33c3545db436f4c3df98bae4fc2'
+    } */
+    const { email, given_name } = ticket.getPayload();
+    //emails are unique for each user
+    let user = await User.findOne({ email });
+    //what if there's not user in the db
+    if (user) {
+      return res.status(400).json({
+        message:
+          "El usuario ya existe, por favor, use otra cuenta o inicie sesión con esa cuenta",
+      });
+    }
+    //create a token
+    user = await User.create({ email, name: given_name });
+    const tokenJWT = newToken(user);
+    res
+      .status(201)
+      .cookie("SECURE_ACCESS", tokenJWT, {
+        httpOnly: true,
+        path: "/",
+        secure: true,
+      })
+      .json({ name: user.name, email: user.email });
+  } catch (e) {
+    res.status(400).json({ message: "No se pudo crear el usuario con google" });
+  }
+}
 /* //function to protect route, example
 export async function protect(req, res) {
   try {
--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,12 +1,21 @@
 import express from "express";
 
 //import controllers
-import { signUp, signIn } from "../controllers/auth.controller";
+import {
+  signUp,
+  signIn,
+  loginGoogle,
+  registerGoogle,
+} from "../controllers/auth.controller";
+import { firstVerify } from "../utils/middlewares/verifyToken";
 //import { authVerify } from "../utils/middlewares/verifyToken";
 const router = express.Router();
 
 router.route("/signin").post(signIn);
 router.route("/signup").post(signUp);
+router.route("/logingoogle").post(loginGoogle);
+router.route("/registergoogle").post(registerGoogle);
+router.route("/verify").get(firstVerify);
 //routers protected
 //model of routers protected
 //router.route("/user").get(authVerify, protect);
